ec64d8819e09e3399338ffe50cbfd7e84913876e
ifysglbiydgfifs <igbsh43hsdh@gmail.com>
2024-09-22 06:27:49 +0500

    1

--- Lab1/Form1.cs
+++ Lab1/Form1.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using System.Windows.Forms.DataVisualization.Charting;
-using ToolTip = System.Windows.Forms.ToolTip;
 
 namespace Lab1
 {
@@ -19,149 +9,95 @@ namespace Lab1
         public Form1()
         {
             InitializeComponent();
+            algorithmListBox.Items.AddRange(new string[]
+            {
+                "Алгоритм 1", "Алгоритм 2", "Алгоритм 3",
+                "Алгоритм 4", "Алгоритм 5", "Алгоритм 6", "Алгоритм 7"
+            });
+            this.Controls.Add(algorithmListBox);
+            buildChartButton.Text = "Построить график";
+            buildChartButton.Click += BuildChartButton_Click;
+            this.Controls.Add(buildChartButton);
 
+        }
+        
+        private void BuildChartButton_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = algorithmListBox.SelectedIndex;
+
+            if (selectedIndex == -1)
+            {
+                MessageBox.Show("Пожалуйста, выберите алгоритм.");
+                return;
+            }
+
+            ClearGraphic();
+
+            double[] time;
+            switch (selectedIndex)
+            {
+                case 0:
+                    time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg1.Run, Convert.ToInt32(textBox_iterations.Text));
+                    break;
+                case 1:
+                    time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg2.Run, Convert.ToInt32(textBox_iterations.Text));
+                    break;
+                case 2:
+                    time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg3.Run, Convert.ToInt32(textBox_iterations.Text));
+                    break;
+                case 3:
+                    time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg4.Run, Convert.ToInt32(textBox_iterations.Text));
+                    break;
+                case 4:
+                    time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg5.BubbleSort, Convert.ToInt32(textBox_iterations.Text));
+                    break;
+                case 5:
+                    time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg6.Run, Convert.ToInt32(textBox_iterations.Text));
+                    break;
+                case 6:
+                    time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg7.Run, Convert.ToInt32(textBox_iterations.Text));
+                    break;
+                default:
+                    MessageBox.Show("Алгоритм не найден.");
+                    return;
+            }
+            
+            for (int i = 0; i < time.Length; i++)
+            {
+                this.chart1.Series[0].Points.AddXY(i + 1, time[i]);
+                this.chart1.Series[1].Points.AddXY(i + 1, (double)i / 700000);  
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
         private void chart1_Click(object sender, EventArgs e)
         {
 
         }
-
-        private void построитьГрафикToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
-        }
-        
-        private ToolTip toolTip1 = new ToolTip();
-
-        private void отчиститьГрафикToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.chart1.Series[0].Points.Clear();
-            this.chart1.Series[1].Points.Clear();
         }
         private void ClearGraphic()
         {
             this.chart1.Series[0].Points.Clear();
             this.chart1.Series[1].Points.Clear();
         }
-
         private void textBox_n_TextChanged(object sender, EventArgs e)
         {
 
         }
-
-        private void алгоритм1ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ClearGraphic();
-            double[] time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg1.Run, Convert.ToInt32(textBox_iterations.Text));
-
-            for (int i = 0; i < time.Length; i++)
-            {
-                this.chart1.Series[0].Points.AddXY(i+1 ,time[i]);
-                this.chart1.Series[1].Points.AddXY(i + 1, (double)i/700000);
-            }
-
-
-
-        } 
-
-        private void алгоритм2ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ClearGraphic();
-            double[] time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg2.Run, Convert.ToInt32(textBox_iterations.Text));
-
-            for (int i = 0; i < time.Length; i++)
-            {
-                this.chart1.Series[0].Points.AddXY(i + 1, time[i]);
-                this.chart1.Series[1].Points.AddXY(i + 1, (double)i / 700000);
-            }
-        }
-
-        private void алгоритм3ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ClearGraphic();
-            double[] time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg3.Run, Convert.ToInt32(textBox_iterations.Text));
-
-            for (int i = 0; i < time.Length; i++)
-            {
-                this.chart1.Series[0].Points.AddXY(i + 1, time[i]);
-                this.chart1.Series[1].Points.AddXY(i + 1, (double)i / 700000);
-            }
-        }
-
-        private void алгоритм4ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ClearGraphic();
-            double[] time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg4.Run, Convert.ToInt32(textBox_iterations.Text));
-
-            for (int i = 0; i < time.Length; i++)
-            {
-                this.chart1.Series[0].Points.AddXY(i + 1, time[i]);
-                this.chart1.Series[1].Points.AddXY(i + 1, (double)i/800000); //при большом n врет
-            }
-        }
-
-        private void алгоритм5ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ClearGraphic();
-            double[] time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg5.BubbleSort, Convert.ToInt32(textBox_iterations.Text));
-
-            for (int i = 0; i < time.Length; i++)
-            {
-                this.chart1.Series[0].Points.AddXY(i + 1, time[i]);
-                this.chart1.Series[1].Points.AddXY(i + 1, Math.Pow(i, 2) / 800000);  //делим для улучшения демонстрации графика
-            }            
-        }
-
-        private void алгоритм6ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ClearGraphic();
-            double[] time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg6.Run, Convert.ToInt32(textBox_iterations.Text));
-
-            for (int i = 0; i < time.Length; i++)
-            {
-                this.chart1.Series[0].Points.AddXY(i + 1, time[i]);
-                this.chart1.Series[1].Points.AddXY(i + 1, 0.00005d);
-            }
-        }
-
-        private void алгоритм7ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ClearGraphic();
-            double[] time = Program.Measure(Program.MakeMassives(Convert.ToInt32(textBox_n.Text)), Alg7.Run, Convert.ToInt32(textBox_iterations.Text));
-
-            for (int i = 0; i < time.Length; i++)
-            {
-                this.chart1.Series[0].Points.AddXY(i + 1, time[i]);
-                this.chart1.Series[1].Points.AddXY(i + 1, 0.00005d);
-            }
-        }
-
-        private void алгоритм8ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
             Program.OnOffSwitch(true);
         }
-
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
             Program.OnOffSwitch(false);
